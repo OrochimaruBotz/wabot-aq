1a4e57d36cb7b7ec70ef0ccdab4624738d81ec8c
Humanz <109642842+OrochimaruBots@users.noreply.github.com>
2023-03-11 12:09:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ global.db = new Low(
 global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
-conn.version = [2, 2143, 3]
+conn.version = [2, 2104, 3]
 let authFile = opts['session'] ? opts['session'] + '.json' : `session.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
